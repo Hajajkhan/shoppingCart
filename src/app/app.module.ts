ba0f82e2c1b85ded34e978a23d1365e65e4d7e12
Hajaj khan <hajajkhan19@gmail.com>
2022-04-20 14:36:06 +0500

    started shoping cart

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FormsModule } from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { DialogComponent } from './navbar/dialog/dialog.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    DialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,FormsModule,MatTableModule,
+    BrowserAnimationsModule,
+    MatDialogModule,MatToolbarModule,MatIconModule,MatInputModule,MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+
+
