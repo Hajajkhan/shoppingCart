e007ed024f94cdfc31c63fe358e22ba4635e34a9
Hajaj khan <hajajkhan19@gmail.com>
2022-04-29 14:59:09 +0500

    cart changing

--- src/app/kifayat.service.ts
+++ src/app/kifayat.service.ts
@@ -10,38 +10,41 @@ export class KifayatService {
   array: any[] = [];
   serachedArray: any[] = [];
   reviews:any[]=[];
-  countedProducts:any;
+  countedProducts:any= this.array.length;
   arrayOfProducts:any;
+  searchedWords:any;
+
+  //searching products By Seaarch API
+  getSearchedData(data: any) {
+    this.searchedWords=data;
+    return this.http
+    .get<any>(
+      'https://backendapi.turing.com/products/search?query_string=' + data
+    )
+    .subscribe((data) => {
+      this.serachedArray = data.rows;
+      console.log("Ff", this.serachedArray);
+    });
+    
+  }
 
   //Main dispatch the action for getting Products
   getProductsData(data: any) {
+    let result;
     if (data != null) {
       return this.http.get(
         'https://backendapi.turing.com/products/inCategory/' +
           data +
           '?limit=50'
-      );
+      )
     } else {
-      return this.http.get('https://backendapi.turing.com/products?limit=100');
+     return this.http.get('https://backendapi.turing.com/products?limit=100');
     }
   }
 
-  //searching products By Seaarch API
-  getSearchedData(data: any) {
-    return this.http
-      .get<any>(
-        'https://backendapi.turing.com/products/search?query_string=' + data
-      )
-      // .subscribe((data) => {
-        
-        // this.serachedArray = data.rows;
-      //   this.serachedArray = data.rows;
-      // });
-  }
-
-
   getDataForSharingToComponents(data: any, arrayOfProducts:any) {
     this.array = data;
+    console.log("Arr", this.array.length);
     this.arrayOfProducts = arrayOfProducts;
   }
 
@@ -49,12 +52,11 @@ export class KifayatService {
   //getting Reviews on the click of view Detail option in SideBar Component
   gettingReviews(data:any){
     return this.http.get<any>("https://backendapi.turing.com/products/"+data+"/reviews").subscribe(data=>{
-      console.log("service", data);
+      console.log("service", this.countedProducts);
       this.reviews=data;
     });
   }
 
   gettingCount(count:any){
-    this.countedProducts=count;
   }
 }
--- src/app/mainpage/navbar/navbar.component.ts
+++ src/app/mainpage/navbar/navbar.component.ts
@@ -1,51 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NavbardialogComponent } from './navbardialog/navbardialog.component';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { KifayatService } from 'src/app/kifayat.service';
-
+import { loadProducts } from '../sidebar/store/kifayat.actions';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
- 
 export class NavbarComponent implements OnInit {
-  
-  searchProduct:any='';
-  arrayOfProducts:any[]=[];
-  prodctdata:any[]=[]
-  countedProducts:any;
-  searchedArray:any[]=[];
-  products:any[]=[];
+  searchProduct: any = '';
+  arrayOfProducts: any[] = [];
+  prodctdata: any[] = [];
+  countedProducts: any;
+  searchedArray: any[] = [];
+  products: any[]=[];
 
   $product: Observable<any> = this.store.select((state: any) => {
     return state.products.products;
   });
 
-
-  constructor(public dialog: MatDialog, private store:Store, private service:KifayatService) { }
-
+  constructor(
+    public dialog: MatDialog,
+    private store: Store,
+    private service: KifayatService
+  ) {}
+  
   ngOnInit(): void {
+    // this.products = this.service.array;
     this.$product.subscribe((data) => {
       this.arrayOfProducts = data;
     });
-    this.countedProducts = this.service.countedProducts;
   }
   openDialog() {
     const dialogRef = this.dialog.open(NavbardialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
+     this.products = this.service.array;
+    console.log('CCC', this.products);
   }
 
-  searchdata(){
-    this.service.getSearchedData(this.searchProduct).subscribe(data=>{
-      this.searchedArray = data.rows;
-      console.log("FF", this.searchedArray);
-    });
-   this.products = this.service.arrayOfProducts;
-   console.log("##", this.products);
+  searchdata() {
+    // this.store.dispatch(loadProducts( {data: this.searchProduct}))
+     this.service.getSearchedData(this.searchProduct);
   }
 }
